9704374707779eebf145b5d8d0ce0a7487a75930
Dean Sutton <d.n.sutton@gmail.com>
2015-11-08 16:01:14 +0000

    Improving configure and Travis CI scripts to display better information.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,23 @@
 #
 # Install the compiler.
-if [ ! -d ~/toolchains/gcc-arm-none-eabi-4_9-2015q3/ ]; then
-   mkdir -p ~/toolchains
-   wget -c https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
+if [ ! -d $HOME/toolchains/gcc-arm-none-eabi-4_9-2015q3/ ]; then
+   mkdir -p $HOME/toolchains
+
+   echo "Downloading toolchain..."
+   wget -c -nv https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
+
+   echo "Installing toolchain..."
    bunzip2 gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2 
    tar -xf gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar 
-   mv gcc-arm-none-eabi-4_9-2015q3 ~/toolchains/
-   export PATH=$PATH:~/toolchains/gcc-arm-none-eabi-4_9-2015q3/bin/
+   mv gcc-arm-none-eabi-4_9-2015q3 $HOME/toolchains/
+   export PATH=$PATH:$HOME/toolchains/gcc-arm-none-eabi-4_9-2015q3/bin/
+
+   echo "System path is now..."
+   echo "PATH=$PATH"
 fi
+
+#
+# Check ARM compiler details
+arm-none-eabi-gcc --version
+arm-none-eabi-g++ --version
+arm-none-eabi-as --version
